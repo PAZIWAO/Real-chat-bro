f68313b42c46d728f0e42f436a690eaf028a4ccb
PAZIWAO <apirak.14233@gmail.com>
2024-10-15 17:24:40 +0700

    Socket url update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000",{
+			const socket = io("https://real-chat-bro.onrender.com/",{
                 query : {
 					userId: authUser._id,
 				},
